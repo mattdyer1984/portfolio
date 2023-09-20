44976cfb306464eae27c5a22a18544447e92dcd1
Apple <apple@matts-mbp-2.home>
2023-09-20 22:00:17 +0100

    modal fixed

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,52 +1,53 @@
 fetch('projects.json')
-.then((response) => {
-    return response.json()
-}).then((data) => {
-    const displayTarget = document.querySelector('#projects')
-    let Projects = ""
-    data.projects.forEach((item) => {
-        Projects +=  
-        '<div><img class="thumbnail" src="' + item.img + '" alt="screen shot of website" width="250px" height="300px"></div>' +
-        '<div><ul>'
-        + '<li>' + '<span class= text-green-500 semi-bold>Name:</span> ' + item.name + '</li><li><span class= text-green-500 semi-bold>Field:</span> ' + item.Field + 
-        '</li><li><span class= text-green-500 semi-bold>Description:</span> ' + item.desc + '</li><li><span class= text-green-500 semi-bold>Link for website :</span><a href="' + item.Links +
-        '" <u>' + item.name + '</u></a></li></ul></div>'+
-        '<div class="modal">'+
-        '<span class="close">&times;</span>'+
-        '<img class="modal-content">'+
-        '</div>'
-        
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    const displayTarget = document.querySelector('#projects');
+    let Projects = "";
 
-    })
-    displayTarget.innerHTML += Projects
-   
-var projects = document.getElementsByClassName("project");
+    data.projects.forEach((item, index) => {
+      Projects +=
+        '<div class="project">' +
+        '<img class="thumbnail" src="' + item.img + '" alt="screen shot of website" width="250px" height="300px">' +
+        '</div>'+
+        '<div>'+
+        '<ul>' +
+        '<li><span class="text-green-500 semi-bold">Name:</span> ' + item.name + '</li>' +
+        '<li><span class="text-green-500 semi-bold">Field:</span> ' + item.Field + '</li>' +
+        '<li><span class="text-green-500 semi-bold">Description:</span> ' + item.desc + '</li>' +
+        '<li><span class="text-green-500 semi-bold">Link for website:</span><a href="' + item.Links + '"><u>' + item.name + '</u></a></li>' +
+        '</ul></div>' +
+        '<div class="modal">' +
+        '<span class="close">&times;</span>' +
+        '<img class="modal-content">' +
+        '</div>';
 
+    });
 
-for (var i = 0; i < projects.length; i++) {
-    var project = projects[i];
-    var modal = project.querySelector(".modal");
-    var modalImg = project.querySelector(".modal-content");
-    var closeModal = project.querySelector(".close");
-    var thumbnail = project.querySelector(".thumbnail");
+    displayTarget.innerHTML += Projects;
 
-    thumbnail.onclick = function() {
-        modal.style.display = "block";
-        modalImg.src = this.src;
-    }
+    let modals = document.querySelectorAll(".modal");
+    let modalImgs = document.querySelectorAll(".modal-content");
+    let closeButtons = document.querySelectorAll(".close");
+    let thumbnails = document.querySelectorAll(".thumbnail");
 
+    thumbnails.forEach((thumbnail, index) => {
+      thumbnail.onclick = function() {
+        modals[index].style.display = "block";
+        modalImgs[index].src = this.src;
+      }
 
-    closeModal.onclick = function() {
-        modal.style.display = "none";
-    }
+      closeButtons[index].onclick = function() {
+        modals[index].style.display = "none";
+      }
+    });
 
-    
     window.onclick = function(event) {
+      modals.forEach((modal) => {
         if (event.target == modal) {
-            modal.style.display = "none";
+          modal.style.display = "none";
         }
+      });
     }
-}
-
-    
-})
+  });
